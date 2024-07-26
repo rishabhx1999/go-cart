eaadf683a686aa4e9ee64a63df941daa396d0930
Rishabh Bansal <rb.14101999.bti@gmail.com>
2024-07-26 17:57:42 +0530

    home promo component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import HomoPromo from './components/Home Promo/HomoPromo';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Hero/>
+      <HomoPromo/>
     </div>
   );
 }
---
+++ src/components/Home Promo/HomoPromo.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import "./HomePromo.css";
+
+const HomePromo = () => {
+  return (
+    <div className="home-promo-wrapper">
+      <div className="home-promo-container">
+        <div className="home-promo-row">
+          <div
+            className="home-promo-kind-one"
+            style={{
+              backgroundImage: `url(${
+                process.env.PUBLIC_URL + "/assets/home-promo-image-1.jpg"
+              })`,
+            }}
+          >
+            <p>Best quality printed tshirts and mugs for all your needs</p>
+          </div>
+          <div
+            className="home-promo-kind-two"
+            style={{
+              backgroundImage: `url(${
+                process.env.PUBLIC_URL + "/assets/home-promo-image-2.jpg"
+              })`,
+            }}
+          ></div>
+          <div
+            className="home-promo-kind-three"
+            style={{
+              backgroundImage: `url(${
+                process.env.PUBLIC_URL + "/assets/home-promo-image-3.jpg"
+              })`,
+            }}
+          >
+            <div className="home-promo-content-kind-one">
+              <h3 className="home-promo-heading-kind-one">
+                Get Printed T-shirt @ $25!
+              </h3>
+              <div className="hero-promo-button-container">
+                <a className="home-promo-button" href="#">
+                  EXPLORE STORE
+                  <img src="/assets/double-right.png" />
+                </a>
+              </div>
+            </div>
+            <div className="home-promo-action"></div>
+          </div>
+        </div>
+        <div className="home-promo-row">
+          <div
+            className="home-promo-kind-four"
+            style={{
+              backgroundImage: `url(${
+                process.env.PUBLIC_URL + "/assets/home-promo-image-4.jpg"
+              })`,
+            }}
+          >
+            <div className="home-promo-content-kind-two">
+              <h5 className="home-promo-heading-kind-two">
+                Get Printed Mug @ $15!
+              </h5>
+              <div className="hero-promo-button-container">
+                <a className="home-promo-button" href="#">
+                  VISIT STORE
+                  <img src="/assets/double-right.png" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            className="home-promo-kind-five"
+            style={{
+              backgroundImage: `url(${
+                process.env.PUBLIC_URL + "/assets/home-promo-image-5.jpg"
+              })`,
+            }}
+          ></div>
+          <div
+            className="home-promo-kind-two"
+            style={{
+              backgroundImage: `url(${
+                process.env.PUBLIC_URL + "/assets/home-promo-image-6.jpg"
+              })`,
+            }}
+          ></div>
+          <div className="home-promo-kind-four">
+            <div className="home-promo-content-kind-three">
+                <img src="/assets/plus-icon.png" alt="" />
+              <h6 className="home-promo-heading-kind-three">Get Your Own</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePromo;
